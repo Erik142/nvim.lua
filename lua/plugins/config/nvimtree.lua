18211d4974e03b54cafcf0798a19fc8015a6d4a0
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-13 11:20:15 +0200

    Add diagnostics in NvimTree

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -1,21 +1,26 @@
-present, nvimtree = pcall(require, "nvim-tree")
+local present, nvimtree = pcall(require, "nvim-tree")
 if not present then return end
 
 nvimtree.setup({
-	open_on_setup = true,
-	renderer = {
-		icons = {
-			glyphs = {
-				git = {
-					unstaged = "",
-					staged = "S",
-					unmerged = "",
-					renamed = "➜",
-					untracked = "U",
-					deleted = "",
-					ignored = "◌"
-				}
-			}
-		}
-	}
+    open_on_setup = true,
+    renderer = {
+        icons = {
+            glyphs = {
+                git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "U",
+                    deleted = "",
+                    ignored = "◌"
+                }
+            }
+        }
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {hint = "", info = "", warning = "", error = ""}
+    }
 })
