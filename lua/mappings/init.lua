d6dafe70aee2618c4319fa3245b1a1eb30addc76
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-13 12:20:20 +0200

    Add lspsaga key bindings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,16 +1,29 @@
 vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap("n", "<leader>c", "<cmd> Bdelete <CR>", {}) -- Close current buffer
-vim.api.nvim_set_keymap("n", "<leader>C", "<C-W>q", {}) -- Close current window
-vim.api.nvim_set_keymap("n", "<leader>v", "<cmd> vsplit <CR>", {}) -- Create vertical split
-vim.api.nvim_set_keymap("n", "<S-L>", "<cmd> BufferLineCycleNext <CR>", {}) -- Go to next buffer
-vim.api.nvim_set_keymap("n", "<S-H>", "<cmd> BufferLineCyclePrev <CR>", {}) -- Go to previous buffer
-vim.api.nvim_set_keymap("n", "<C-L>", "<C-W>l", {}) -- Go to next buffer
-vim.api.nvim_set_keymap("n", "<C-H>", "<C-W>h", {}) -- Go to previous buffer
-vim.api.nvim_set_keymap("n", "<C-J>", "<C-W>j", {}) -- Go to next buffer
-vim.api.nvim_set_keymap("n", "<C-K>", "<C-W>k", {}) -- Go to previous buffer
-vim.api.nvim_set_keymap("n", "<leader>e", "<cmd> NvimTreeToggle <CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>f", "<cmd> Telescope find_files <CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd> Telescope live_grep <CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd> Telescope grep_string <CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>h", "<cmd> Telescope help_tags <CR>", {})
+vim.keymap.set("n", "<leader>c", "<cmd> Bdelete <CR>", {}) -- Close current buffer
+vim.keymap.set("n", "<leader>C", "<C-W>q", {}) -- Close current window
+vim.keymap.set("n", "<leader>v", "<cmd> vsplit <CR>", {}) -- Create vertical split
+vim.keymap.set("n", "<S-L>", "<cmd> BufferLineCycleNext <CR>", {}) -- Go to next buffer
+vim.keymap.set("n", "<S-H>", "<cmd> BufferLineCyclePrev <CR>", {}) -- Go to previous buffer
+vim.keymap.set("n", "<C-L>", "<C-W>l", {}) -- Go to next buffer
+vim.keymap.set("n", "<C-H>", "<C-W>h", {}) -- Go to previous buffer
+vim.keymap.set("n", "<C-J>", "<C-W>j", {}) -- Go to next buffer
+vim.keymap.set("n", "<C-K>", "<C-W>k", {}) -- Go to previous buffer
+vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeToggle <CR>", {})
+vim.keymap.set("n", "<leader>f", "<cmd> Telescope find_files <CR>", {})
+vim.keymap.set("n", "<leader>lg", "<cmd> Telescope live_grep <CR>", {})
+vim.keymap.set("n", "<leader>gs", "<cmd> Telescope grep_string <CR>", {})
+vim.keymap.set("n", "<leader>h", "<cmd> Telescope help_tags <CR>", {})
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", {silent = true})
+vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>",
+               {silent = true})
+vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>",
+               {silent = true})
+vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>",
+               {silent = true})
+vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>",
+               {silent = true})
+vim.keymap.set("n", "gs", "<Cmd>Lspsaga signature_help<CR>", {silent = true})
+vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", {silent = true})
+vim.keymap
+    .set("n", "gd", "<cmd>Lspsaga preview_definition<CR>", {silent = true})
