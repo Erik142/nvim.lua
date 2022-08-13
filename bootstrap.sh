a961d07efdc5be46d71563e1bc43b8a1a33b1e5a
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-13 10:53:47 +0200

    Add formatting and diagnostics for lua and yaml

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+luarocks install --server=https://luarocks.org/dev luaformatter --local
+yarn global add prettier
--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -3,6 +3,8 @@ if not present then return end
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.lua_format
+		null_ls.builtins.formatting.lua_format,
+		null_ls.builtins.diagnostics.yamllint,
+		null_ls.builtins.formatting.prettier
 	}
 })
