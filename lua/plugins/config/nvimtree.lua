7d3994666053429fa9bb98a0cb879342c70e2038
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-13 10:35:32 +0200

    Update NvimTree git icons

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -3,4 +3,19 @@ if not present then return end
 
 nvimtree.setup({
 	open_on_setup = true,
+	renderer = {
+		icons = {
+			glyphs = {
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "U",
+					deleted = "",
+					ignored = "◌"
+				}
+			}
+		}
+	}
 })
