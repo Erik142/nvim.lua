755c6c2ff60149d1b6c36b02683836c4437324ed
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-14 12:11:08 +0200

    Add more markdown default opts

--- lua/plugins/config/nvim-markdown.lua
+++ lua/plugins/config/nvim-markdown.lua
@@ -1,5 +1,6 @@
 -- disable header folding
 vim.g.vim_markdown_folding_disabled = 1
+vim.cmd('let g:pandoc#modules#disabled = ["folding"]')
 
 -- do not use conceal feature, the implementation is not so good
 vim.g.vim_markdown_conceal = 0
@@ -7,6 +8,8 @@ vim.g.vim_markdown_conceal = 0
 -- disable math tex conceal feature
 vim.g.tex_conceal = ""
 vim.g.vim_markdown_math = 1
+
+vim.g.vim_markdown_no_default_key_mappings = 1
 
 vim.cmd([[augroup pandoc_syntax
     au! BufNewFile,BufFilePre,BufRead *.md set filetype=markdown.pandoc
