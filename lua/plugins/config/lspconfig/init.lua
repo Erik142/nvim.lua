6d8255ecda78b81a326548190a4ef5a7e5d5b130
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-15 07:53:18 +0200

    Add erlangls language server

--- lua/plugins/config/lspconfig/init.lua
+++ lua/plugins/config/lspconfig/init.lua
@@ -1,4 +1,5 @@
 require("plugins.config.lspconfig.dockerls")
+require("plugins.config.lspconfig.erlangls")
 require("plugins.config.lspconfig.prismals")
 require("plugins.config.lspconfig.pyright")
 require("plugins.config.lspconfig.rust_analyzer")
