e96f0ce924a6d710f6161cbb19a447546af3a8e9
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-15 09:31:51 +0200

    Change telescope default layout to horizontal

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -1,8 +1,9 @@
 local present, telescope = pcall(require, "telescope")
 if not present then return end
 
-telescope.setup{
-	defaults = {
-		layout_strategy = 'cursor'
-	}
+telescope.setup {
+    defaults = {
+        layout_strategy = 'horizontal',
+        layout_config = {horizontal = {prompt_position = 'top'}}
+    }
 }
