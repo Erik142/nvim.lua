ce0e2ce137e64116822e55056364216d66eb3cd7
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-15 09:34:14 +0200

    Add NvimTree settings for project plugin

--- lua/plugins/config/nvimtree.lua
+++ lua/plugins/config/nvimtree.lua
@@ -22,5 +22,8 @@ nvimtree.setup({
         enable = true,
         show_on_dirs = true,
         icons = {hint = "", info = "", warning = "", error = ""}
-    }
+    },
+    sync_root_with_cwd = true,
+    respect_buf_cwd = true,
+    update_focused_file = {enable = true, update_root = true}
 })
