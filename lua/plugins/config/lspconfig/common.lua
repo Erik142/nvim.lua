4399d6e3aa33b4ee6a78b024c98649ef7d8d160e
Erik Wahlberger <ewahlberger@gmail.com>
2022-12-03 20:01:11 +0100

    Fix deprecated lsp functions

--- lua/plugins/config/lspconfig/common.lua
+++ lua/plugins/config/lspconfig/common.lua
@@ -3,62 +3,62 @@ local protocol = require('vim.lsp.protocol')
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 On_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 
-    -- formatting
-    if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("Format", {clear = true}),
-            buffer = bufnr,
-            callback = function() vim.lsp.buf.formatting_seq_sync() end
-        })
-    end
+	-- formatting
+	if client.server_capabilities.documentFormattingProvider then
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = vim.api.nvim_create_augroup("Format", { clear = true }),
+			buffer = bufnr,
+			callback = function() vim.lsp.buf.format { async = true } end
+		})
+	end
 end
 
 protocol.CompletionItemKind = {
-    '', -- Text
-    '', -- Method
-    '', -- Function
-    '', -- Constructor
-    '', -- Field
-    '', -- Variable
-    '', -- Class
-    'ﰮ', -- Interface
-    '', -- Module
-    '', -- Property
-    '', -- Unit
-    '', -- Value
-    '', -- Enum
-    '', -- Keyword
-    '﬌', -- Snippet
-    '', -- Color
-    '', -- File
-    '', -- Reference
-    '', -- Folder
-    '', -- EnumMember
-    '', -- Constant
-    '', -- Struct
-    '', -- Event
-    'ﬦ', -- Operator
-    '' -- TypeParameter
+	'', -- Text
+	'', -- Method
+	'', -- Function
+	'', -- Constructor
+	'', -- Field
+	'', -- Variable
+	'', -- Class
+	'ﰮ', -- Interface
+	'', -- Module
+	'', -- Property
+	'', -- Unit
+	'', -- Value
+	'', -- Enum
+	'', -- Keyword
+	'﬌', -- Snippet
+	'', -- Color
+	'', -- File
+	'', -- Reference
+	'', -- Folder
+	'', -- EnumMember
+	'', -- Constant
+	'', -- Struct
+	'', -- Event
+	'ﬦ', -- Operator
+	'' -- TypeParameter
 }
 
 -- Set up completion using nvim_cmp with LSP source
-Capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol
-                                                               .make_client_capabilities())
+Capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
+	.make_client_capabilities())
