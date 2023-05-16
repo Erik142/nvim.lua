46cdf6fdffb8c8e9dd82751cf3d5562fe11513d6
Erik Wahlberger <ewahlberger@gmail.com>
2023-05-16 20:21:44 +0200

    add gruvbox theme

---
+++ lua/custom/plugins/gruvbox.lua
@@ -0,0 +1,8 @@
+return {
+	-- Theme inspired by Atom
+	'luisiacc/gruvbox-baby',
+	config = function()
+		vim.g.gruvbox_baby_use_original_palette = true
+		vim.cmd.colorscheme 'gruvbox-baby'
+	end,
+}
