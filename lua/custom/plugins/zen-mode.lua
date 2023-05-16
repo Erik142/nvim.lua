e3b7b1938e0b9f999c88620b4b285f2886f882d0
Erik Wahlberger <ewahlberger@gmail.com>
2023-05-16 21:33:02 +0200

    add zen-mode plugin

---
+++ lua/custom/plugins/zen-mode.lua
@@ -0,0 +1,12 @@
+return {
+	'folke/zen-mode.nvim',
+	dependencies = { { 'folke/twilight.nvim', config = true } },
+	opts = {
+		plugins = {
+			twilight = { enabled = true },
+			gitsigns = { enabled = false },
+			tmux = { enabled = false },
+			options = { enabled = true, ruler = false }
+		}
+	}
+}
