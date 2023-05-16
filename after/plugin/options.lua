f806a50824587068cae4c680d45016dd6c802750
Erik Wahlberger <ewahlberger@gmail.com>
2023-05-16 21:30:57 +0200

    add scrolloff = 10 to options

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -5,6 +5,7 @@ vim.o.tabstop = 2
 vim.o.expandtab = true
 vim.o.splitbelow = true
 vim.o.splitright = true
+vim.o.scrolloff = 10
 
 vim.cmd([[if exists("g:neovide")
 	let g:neovide_cursor_animation_length=0
