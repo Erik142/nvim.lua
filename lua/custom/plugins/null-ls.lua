521ec847bab486fb15ecbc36756f59bf028ea85a
Erik Wahlberger <ewahlberger@gmail.com>
2023-05-16 21:32:15 +0200

    add null-ls plugin

---
+++ lua/custom/plugins/null-ls.lua
@@ -0,0 +1,13 @@
+return {
+	'jose-elias-alvarez/null-ls.nvim',
+	config = function()
+		local null_ls = require 'null-ls'
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.lua_format,
+				null_ls.builtins.diagnostics.yamllint,
+				null_ls.builtins.formatting.prettier, null_ls.builtins.formatting.gofmt
+			}
+		})
+	end,
+}
