fb9ff8e652425a655de6eab32270a6da1f659f75
Erik Wahlberger <ewahlberger@gmail.com>
2024-09-27 17:34:20 +0200

    chore(plugins): format zen-mode.lua

--- lua/custom/plugins/zen-mode.lua
+++ lua/custom/plugins/zen-mode.lua
@@ -1,13 +1,13 @@
 return {
-	'folke/zen-mode.nvim',
-	dependencies = { { 'folke/twilight.nvim', config = true } },
-	opts = {
-		plugins = {
-			twilight = { enabled = true },
-			gitsigns = { enabled = false },
-			tmux = { enabled = false },
-			options = { enabled = true, ruler = false }
-		}
-	},
-	lazy = true
+    'folke/zen-mode.nvim',
+    dependencies = {{'folke/twilight.nvim', config = true}},
+    opts = {
+        plugins = {
+            twilight = {enabled = true},
+            gitsigns = {enabled = false},
+            tmux = {enabled = false},
+            options = {enabled = true, ruler = false}
+        }
+    },
+    lazy = false
 }
