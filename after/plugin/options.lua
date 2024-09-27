4d2d18859be6a21d289e5971ede91e98d798cad3
Erik Wahlberger <ewahlberger@gmail.com>
2024-09-27 17:28:53 +0200

    fix(options): remove swapfile

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -6,6 +6,7 @@ vim.o.expandtab = true
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.scrolloff = 10
+vim.o.swapfile = false
 
 vim.cmd([[if exists("g:neovide")
 	let g:neovide_cursor_animation_length=0
