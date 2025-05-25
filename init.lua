eeafe96cd6b8236974dd1a5e4c2142ca33a5f7fc
Erik Wahlberger <ewahlberger@gmail.com>
2025-05-25 19:38:34 +0200

    chore: remove zk lsp
    This is installed automatically when the zk cli is installed on the
    host.

--- init.lua
+++ init.lua
@@ -682,7 +682,6 @@ require('lazy').setup({
         rust_analyzer = {},
         tflint = {},
         yamlls = {},
-        zk = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
