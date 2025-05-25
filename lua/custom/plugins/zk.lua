ac4f757252b64aa7cc655454ed52bc1373a7eeb9
Erik Wahlberger <ewahlberger@gmail.com>
2025-05-25 19:39:23 +0200

    fix(zk-nvim): use snacks_picker

--- lua/custom/plugins/zk.lua
+++ lua/custom/plugins/zk.lua
@@ -1,4 +1,6 @@
 return {
-    "zk-org/zk-nvim",
-    config = function() require("zk").setup({picker = "telescope"}) end
+  'zk-org/zk-nvim',
+  config = function()
+    require('zk').setup { picker = 'snacks_picker' }
+  end,
 }
