1cac5c541e48aba662d9873512a2066a9e05f2a5
Erik Wahlberger <ewahlberger@gmail.com>
2025-05-25 19:37:17 +0200

    fix: restore old custom options/mapping behavior

--- init.lua
+++ init.lua
@@ -1023,3 +1023,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+require './after.plugin.options'
+require './after.plugin.mappings'
