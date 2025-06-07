eb91e468719341f54e14cb8ccd09335fc34e00e4
Erik Wahlberger <ewahlberger@gmail.com>
2025-06-07 15:44:05 +0200

    chore(mappings): update formatting

--- lua/after/plugin/mappings.lua
+++ lua/after/plugin/mappings.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("i", "kj", "<Esc>", {})
-vim.keymap.set("i", "jk", "<Esc>", {})
+vim.keymap.set('i', 'kj', '<Esc>', {})
+vim.keymap.set('i', 'jk', '<Esc>', {})
+vim.keymap.set('v', '<leader>ft', "<CMD>!column -t -s '|'<CR>", {})
